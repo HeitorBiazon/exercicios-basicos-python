4758ac039be12c8dbef9e86de00ac3e18ea98343
Heitor Biazon <biazonheitor@gmail.com>
2024-06-08 22:20:06 -0300

    Exercício 4

--- main.py
+++ main.py
@@ -65,3 +65,13 @@ for i in lista1:
           lista2.append(i)
 print(lista2)
 # %%
+'''Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)'''
+
+## Exibir os divisores abaixo de 1000 de um determinado número 
+
+numero = int(input("Digite um número: "))
+divisores = []
+for i in range(1, numero + 1):
+     if numero % i == 0:
+          divisores.append(i)
+print(divisores)
